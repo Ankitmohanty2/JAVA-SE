cf0dd775ab9938a4fc62af90e5109d2aa7848bd1
Ankit Mohanty <117025304+Ankitmohanty2@users.noreply.github.com>
2024-07-08 21:31:26 +0530

    Create arraymerging.java

---
+++ Arrays/arraymerging.java
@@ -0,0 +1,25 @@
+public class MergeArrays {
+    public static void main(String[] args) {
+        int[] array1 = {1, 2, 3};
+        int[] array2 = {4, 5, 6};
+        
+        int length = array1.length + array2.length;
+        int[] mergedArray = new int[length];
+        
+        int pos = 0;
+        for (int element : array1) {
+            mergedArray[pos] = element;
+            pos++;
+        }
+        
+        for (int element : array2) {
+            mergedArray[pos] = element;
+            pos++;
+        }
+        
+        // Print the merged array
+        for (int element : mergedArray) {
+            System.out.print(element + " ");
+        }
+    }
+}
