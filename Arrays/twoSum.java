650294ce78eee9f56ae2787b0d81316ec638831b
Ankit Mohanty <117025304+Ankitmohanty2@users.noreply.github.com>
2024-08-02 14:23:23 +0530

    Update twoSum.java

--- Arrays/twoSum.java
+++ Arrays/twoSum.java
@@ -28,7 +28,7 @@ public class Solution {
     }
     
     public static void main(String[] args) {
-        // Example input
+      
         int[] nums = {2, 7, 11, 15};
         int target = 9;
         
