cce8261e2c116233827ccdc83c019cdbcd10e89c
Ankit Mohanty <117025304+Ankitmohanty2@users.noreply.github.com>
2024-08-12 18:00:56 +0530

    Update PrefixArray.java

--- Arrays/PrefixArray.java
+++ Arrays/PrefixArray.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 /**
- * @author Vishal Singh */
+ * @author Ankit */
 public class PrefixSumArray {
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
