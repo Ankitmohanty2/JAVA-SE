b1de6f82da4838795019cefed025c265c917c005
Ankit Mohanty <117025304+Ankitmohanty2@users.noreply.github.com>
2024-08-14 16:23:49 +0530

    Update ArrayLists.java

--- Arrays/ArrayLists.java
+++ Arrays/ArrayLists.java
@@ -1,43 +1,82 @@
+
+/*-- Comparing two lists' objects --*/
+
 import java.util.*;
 
 public class ArraysLists {
 
     static List<Integer> compareLists(List<Integer> a, List<Integer> b) {
-        // The arrays are converted to lists directly
-        Integer[] arr1 = a.toArray(new Integer[a.size()]);
-        Integer[] arr2 = b.toArray(new Integer[b.size()]);
 
-        int aaa = 0; // Count of elements in `a` greater than in `b`
-        int bbb = 0; // Count of elements in `b` greater than in `a`
+        List<Integer> list1 = new ArrayList<Integer>();
+        list1 = a;
 
-        // Compare corresponding elements of both arrays
-        for (int i = 0; i < a.size(); i++) {
-            if (arr1[i] > arr2[i]) {
-                aaa++;
-            } else if (arr1[i] < arr2[i]) {
-                bbb++;
-            }
+        List<Integer> list2 = new ArrayList<Integer>();
+        list2 = b;
+
+        /* This line is tricky */
+
+        /*
+        
+        Integer[] arr1 = new Integer[list1.size];
+        arr1 = list1.toArray();
+        The above two lines can be merged and can be written as:
+        Integer[] arr1 = list1.toArray(new Integer[list1.size()]);
+
+        */
+
+        Integer[] arr1 = list1.toArray(new Integer[list1.size()]);
+
+        Integer[] arr2 = list2.toArray(new Integer[list2.size()]);
+
+        int aaa=0;
+        int bbb=0;
+        
+        for(int i=0; i<list1.size(); i++){
+
+            if(arr1[i]==arr2[i])
+            {}
+
+            else if(arr1[i]>arr2[i])
+            aaa++;
+
+            else if(arr1[i]<arr2[i])
+            bbb++;
         }
 
-        // Create and return a list containing the counts
-        List<Integer> arrFinal = new ArrayList<>();
+        List<Integer> arrFinal = new ArrayList<Integer>();
         arrFinal.add(aaa);
         arrFinal.add(bbb);
 
+
         return arrFinal;
-    }
 
-    public static void main(String[] args) {
-        // Initialize lists with the desired elements
-        List<Integer> A = new ArrayList<>(Arrays.asList(20, 30, 40, 50));
-        List<Integer> B = new ArrayList<>(Arrays.asList(40, 40, 40, 40));
-
-        // Compare the lists
-        List<Integer> result = compareLists(A, B);
-
-        // Output the results
-        for (int i = 0; i < result.size(); i++) {
-            System.out.println(result.get(i));
-        }
-    }
 }
+    
+    /*-- Inputs from the main method --*/
+    
+    public static void main(String[] args){
+    
+        List<Integer> A = new ArrayList<>(new Integer(10));
+        A.add(20);
+        A.add(30);
+        A.add(40);
+        A.add(50);
+        
+        List<Integer> B = new ArrayList<>(new Integer(30));
+        B.add(40);
+        B.add(40);
+        B.add(40);
+        B.add(40);
+        
+        List<Integer> list = compareLists(A,B);
+        
+        for(int i=0; i<list.size(); i++){
+            System.out.println(list.get(i));
+        }
+        
+    }
+    
+}
+
+
+
